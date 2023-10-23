6bd9966f8c556d08014c4c40785c075a5b813666
IronLegends7519 <drafin@edenschool.fr>
2023-10-23 15:19:12 +0200

    créer js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let Price = document.querySelector("#price")
+let Name = document.querySelector("#name")
+let Revelance = document.querySelector("#revelance") 
 
 
+Price.addEventListener("click", ()=>{
+  Price.style.backgroundColor="aqua";
+  Name.style.backgroundColor="white";
+  Revelance.style.backgroundColor="white";
+})
+Name.addEventListener("click", ()=>{
+  Name.style.backgroundColor="aqua";
+  Price.style.backgroundColor="white";
+  Revelance.style.backgroundColor="white";
+})
+Revelance.addEventListener("click", ()=>{
+  Revelance.style.backgroundColor="aqua";
+  Name.style.backgroundColor="white";
+  Price.style.backgroundColor="white";
+})
